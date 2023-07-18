a4ea611c58008b85506137c07e6e0b3f3d0dfef5
Leeyanhawrt <leeyan1108@gmail.com>
2023-07-18 19:45:23 -0400

    Add picsum api call to generate random images

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
 
   return (
     <div className="app">
+      <h1>Reading List</h1>
       <BookCreate onCreate={createBook} />
       <BookList books={books} onEdit={editBookById} onDelete={deleteBookById} />
     </div>
