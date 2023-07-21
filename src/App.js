96328b3ba5110a481ba7de9ad36710549683d7ae
Leeyanhawrt <leeyan1108@gmail.com>
2023-07-20 22:38:51 -0400

    Begin learning about context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import { useState } from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
+
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    setBooks([...books, { title, id: Math.round(Math.random() * 9999) }])
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books')
+
+    setBooks(response.data)
+  };
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    });
+
+    setBooks([...books, response.data])
   }
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`)
     setBooks(books.filter((book) => {
       return book.id !== id;
     }))
   }
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,18 @@
 import BookShow from "./BookShow";
+import { useContext } from "react"
+import BooksContext from "../context/books";
 
 function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
   })
 
   return (
     <div className="book-list">
+      {count}
+      <button onClick={incrementCount}></button>
       {renderedBooks}
     </div>
   )
